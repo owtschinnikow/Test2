67bb0b3212e85ed15fed7cffc3c23494b7170d2f
Serg Owtschinnikow <owtschinnikow@rambler.ru>
2021-11-13 02:06:37 +0300

    add test fib()

--- lec_13/fib.py
+++ lec_13/fib.py
@@ -1,5 +1,27 @@
 def fib(n: int):
+    """
+    Функция вычисления фибинаци
+    >>> fib(1)
+    1
+    >>> fib(2)
+    1
+    >>> fib(3)
+    2
+    >>> fib(5)
+    5
+    >>> fib(10)
+    55
+    >>> fib(20)
+    6765
+
+    :param n:
+    :return:
+    """
     if n < 2:
         return n
     else:
         return fib(n-1) + fib(n-2)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose = True)
---
+++ lec_13/test fib.py
@@ -0,0 +1,17 @@
+import fib
+
+
+all_correct = True
+
+for n, answer in [(0, 0), (1, 1), (2, 1), (5, 10)]:
+    result = fib.fib(n)
+    correct = (result == answer)
+    if not correct:
+        print('Test case failed: n =', n, result, '!=', answer)
+    all_correct &= correct
+
+
+if all_correct:
+    print('Testing fib(): OK')
+else:
+    print('Testing fib(): Failed')
